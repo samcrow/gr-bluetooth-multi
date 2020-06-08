b5e502ced8d37c6ed6d6b3112905c78855ff79f9
Florian Eckert <florian.eckert@ymail.com>
2020-06-08 13:10:55 +0200

    fixed issue with swig

--- python/__init__.py
+++ python/__init__.py
@@ -3,7 +3,7 @@
 #
 # This application is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
+# the Free Software Foundation; either version 3, or (at your option)
 # any later version.
 #
 # This application is distributed in the hope that it will be useful,
@@ -22,33 +22,14 @@
 This is the GNU Radio BLUETOOTH module. Place your Python package
 description here (python/__init__.py).
 '''
+from __future__ import unicode_literals
 
-# ----------------------------------------------------------------
-# Temporary workaround for ticket:181 (swig+python problem)
-import sys
-_RTLD_GLOBAL = 0
+# import swig generated symbols into the gr_bluetooth namespace
 try:
-	from dl import RTLD_GLOBAL as _RTLD_GLOBAL
+    # this might fail if the module is python-only
+    from .gr_bluetooth_swig import *
 except ImportError:
-	try:
-		from DLFCN import RTLD_GLOBAL as _RTLD_GLOBAL
-	except ImportError:
-		pass
-
-if _RTLD_GLOBAL != 0:
-	_dlopenflags = sys.getdlopenflags()
-	sys.setdlopenflags(_dlopenflags|_RTLD_GLOBAL)
-# ----------------------------------------------------------------
-
-
-# import swig generated symbols into the bluetooth namespace
-from gr_bluetooth import *
+    pass
 
 # import any pure python here
 #
-
-# ----------------------------------------------------------------
-# Tail of workaround
-if _RTLD_GLOBAL != 0:
-	sys.setdlopenflags(_dlopenflags)      # Restore original flags
-# ----------------------------------------------------------------
